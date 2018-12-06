44a32edd01744cfa3709f443a255b5507d2f982f
keegan <keegan.millard@gmail.com>
2018-12-06 12:51:15 -0700

    new tests

---
+++ Makefile
@@ -0,0 +1,12 @@
+export opts= -std=c++14 -Werror -Wfatal-errors
+            
+CXXFLAGS= -O3 -Wall $$opts
+
+PROJECT_DIR= '/Users/keegan/CS253/cs253hw/hw9/'
+LIB_PATH='/Users/keegan/CS253/cs253hw/hw9/cmake-build-debug/libhw8.a'
+
+test: $(lib)
+		$(CXX) $(CXXFLAGS) -I $(PROJECT_DIR) speed_test.cpp $(LIB_PATH) -o speed_test
+
+clean:
+		rm -f  *.o *.gch speed_test
--- speed_test.cpp
+++ speed_test.cpp
@@ -7,6 +7,7 @@
 #include <fstream>
 #include <unordered_map>
 
+#include "Image.h"
 #include "PGM.h"
 #include "Alpha.h"
 
@@ -25,7 +26,7 @@ void create(string filename) {
         delete im;
 }
 
-void write(const string &filename) {
+void write(const string &filename){
     Image *im = image_map[filename];
     im->write(destdir+filename);
 }
@@ -40,56 +41,83 @@ void double_size(const string &filename){
     im->resize(2.0);
 }
 
-struct TestObject {
-    const string name;
+void mirror(const string &filename){
+    Image *im = image_map[filename];
+    im->mirror();
+}
 
-    void run_test(const string &name, int iter) {
-        using namespace std::chrono;
-        auto start = high_resolution_clock::now();
+void rotate90(const string &filename){
+    Image *im = image_map[filename];
+    im->rotate(90);
+}
 
-        for(int x = 0; x < iter; x++)
-            test();
+void rotate180(const string &filename){
+    Image *im = image_map[filename];
+    im->rotate(180);
+}
 
-        auto duration =  high_resolution_clock::now() - start;
-        std::cout << name << " x" << to_string(iter) << '\n';
-        std::cout << "ms: " << duration_cast<milliseconds>(duration).count() << '\n';
-    }
+void rotate270(const string &filename){
+    Image *im = image_map[filename];
+    im->rotate(270);
+}
 
-    virtual void test() = 0;
+void min(int dir, const string &filename1, const string &filename2){
+    Image *im1 = image_map[filename1];
+    Image *im2 = image_map[filename1];
+    im1->min(dir,*im2);
+}
 
-};
+void max(int dir, const string &filename1, const string &filename2){
+    Image *im1 = image_map[filename1];
+    Image *im2 = image_map[filename1];
+    im1->max(dir,*im2);
+}
 
-struct CreateSmallAlpha : public TestObject {
+template<typename T>
+void run_test(const string &name, int iter) {
+    using namespace std::chrono;
+    auto start = high_resolution_clock::now();
 
-    void test() override {
+    for(int x = 0; x < iter; x++)
+        T::test();
+
+    auto duration =  high_resolution_clock::now() - start;
+    std::cout << name << " x " << to_string(iter) << '\n';
+    std::cout << "ms: " << duration_cast<milliseconds>(duration).count() << '\n';
+}
+
+struct CreateSmallAlpha {
+
+    static void test() {
 //        create("j.alpha");
 //        create("j_clean.alpha");
 //        create("j_star.alpha");
-        create("p.alpha");
+//        create("p.alpha");
 //        create("hi.alpha");
         create("feep.alpha");
-        create("luna_small.alpha");
+//        create("luna_small.alpha");
         create("image10_small.alpha");
+        create("image11_small.alpha");
         create("image15_small.alpha");
 
     }
 };
 
-struct CreateSmallPGM : public TestObject {
+struct CreateSmallPGM {
 
-    void test() override {
-        create("hi.pgm");
+    static void test() {
+//        create("hi.pgm");
         create("feep.pgm");
-        create("my.pgm");
+ //      create("my.pgm");
         create("image10_small.pgm");
         create("image11_small.pgm");
         create("image15_small.pgm");
     }
 };
 
-struct CreateLargePGM : public TestObject {
+struct CreateLargePGM {
 
-    void test() override {
+    static void test() {
         create("einstein.pgm");
         create("image10.pgm");
         create("image16.pgm");
@@ -102,9 +130,9 @@ struct CreateLargePGM : public TestObject {
 
 };
 
-struct CreateLargeAlpha : public TestObject {
+struct CreateLargeAlpha {
 
-    void test() override {
+    static void test() {
         create("einstein.alpha");
         create("image10.alpha");
         create("image16.alpha");
@@ -117,9 +145,9 @@ struct CreateLargeAlpha : public TestObject {
 
 };
 
-struct WriteSmallAlpha : public TestObject {
+struct WriteSmallAlpha {
 
-    void test() override {
+    static void test() {
 //        create("j.alpha");
 //        create("j_clean.alpha");
 //        create("j_star.alpha");
@@ -128,14 +156,15 @@ struct WriteSmallAlpha : public TestObject {
         write("feep.alpha");
 //        write("luna_small.alpha");
         write("image10_small.alpha");
+        write("image11_small.alpha");
         write("image15_small.alpha");
 
     }
 };
 
-struct WriteSmallPGM : public TestObject {
+struct WriteSmallPGM {
 
-    void test() override {
+    static void test() {
 //        create("j.pgm");
 //        create("j_clean.pgm");
 //        create("j_star.pgm");
@@ -144,14 +173,15 @@ struct WriteSmallPGM : public TestObject {
         write("feep.pgm");
 //        write("luna_small.pgm");
         write("image10_small.pgm");
+        write("image11_small.pgm");
         write("image15_small.pgm");
 
     }
 };
 
-struct WriteLargeAlpha : public TestObject {
+struct WriteLargeAlpha {
 
-    void test() override {
+    static void test() {
         write("einstein.alpha");
         write("image10.alpha");
         write("image16.alpha");
@@ -164,9 +194,9 @@ struct WriteLargeAlpha : public TestObject {
 
 };
 
-struct WriteLargePGM : public TestObject {
+struct WriteLargePGM {
 
-    void test() override {
+    static void test() {
         write("einstein.pgm");
         write("image10.pgm");
         write("image16.pgm");
@@ -179,50 +209,216 @@ struct WriteLargePGM : public TestObject {
 
 };
 
-struct ResizePGM : public TestObject {
+struct Resize {
 
-    void test() override {
+    static void test() {
         half_size("einstein.pgm");
         half_size("image10.pgm");
         half_size("image16.pgm");
         half_size("image17.pgm");
         half_size("image21.pgm");
-        half_size("image23.pgm");
-        half_size("image24.pgm");
-        half_size("image26.pgm");
+
+        half_size("einstein.alpha");
+        half_size("image10.alpha");
+        half_size("image16.alpha");
+        half_size("image17.alpha");
+        half_size("image21.alpha");
 
         double_size("einstein.pgm");
         double_size("image10.pgm");
         double_size("image16.pgm");
         double_size("image17.pgm");
         double_size("image21.pgm");
-        double_size("image23.pgm");
-        double_size("image24.pgm");
-        double_size("image26.pgm");
-    }
-
-};
-
-struct ResizeAlpha : public TestObject {
-
-    void test() override {
-        half_size("einstein.alpha");
-        half_size("image10.alpha");
-        half_size("image16.alpha");
-        half_size("image17.alpha");
-        half_size("image21.alpha");
-        half_size("image23.alpha");
-        half_size("image24.alpha");
-        half_size("image26.alpha");
 
         double_size("einstein.alpha");
         double_size("image10.alpha");
         double_size("image16.alpha");
         double_size("image17.alpha");
         double_size("image21.alpha");
-        double_size("image23.alpha");
-        double_size("image24.alpha");
-        double_size("image26.alpha");
+    }
+
+};
+
+struct Mirror {
+
+    static void test() {
+        mirror("feep.pgm");
+        mirror("image10_small.pgm");
+        mirror("image11_small.pgm");
+        mirror("image15_small.pgm");
+
+        mirror("einstein.pgm");
+        mirror("image10.pgm");
+        mirror("image16.pgm");
+        mirror("image17.pgm");
+        mirror("image21.pgm");
+
+        mirror("feep.alpha");
+        mirror("image10_small.alpha");
+        mirror("image11_small.alpha");
+        mirror("image15_small.alpha");
+
+        mirror("einstein.alpha");
+        mirror("image10.alpha");
+        mirror("image16.alpha");
+        mirror("image17.alpha");
+        mirror("image21.alpha");
+    }
+
+};
+
+struct Rotate90 {
+
+    static void test() {
+        rotate90("feep.pgm");
+        rotate90("image10_small.pgm");
+        rotate90("image11_small.pgm");
+        rotate90("image15_small.pgm");
+        rotate90("einstein.pgm");
+        rotate90("image10.pgm");
+        rotate90("image16.pgm");
+        rotate90("image17.pgm");
+        rotate90("image21.pgm");
+        rotate90("image23.pgm");
+        rotate90("image24.pgm");
+        rotate90("image26.pgm");
+
+        rotate90("feep.alpha");
+        rotate90("image10_small.alpha");
+        rotate90("image11_small.alpha");
+        rotate90("image15_small.alpha");
+        rotate90("einstein.alpha");
+        rotate90("image10.alpha");
+        rotate90("image16.alpha");
+        rotate90("image17.alpha");
+        rotate90("image21.alpha");
+        rotate90("image23.alpha");
+        rotate90("image24.alpha");
+        rotate90("image26.alpha");
+    }
+
+};
+
+struct Rotate180 {
+
+    static void test() {
+        rotate180("feep.pgm");
+        rotate180("image10_small.pgm");
+        rotate180("image15_small.pgm");
+        rotate180("einstein.pgm");
+        rotate180("image10.pgm");
+        rotate180("image16.pgm");
+        rotate180("image17.pgm");
+        rotate180("image21.pgm");
+        rotate180("image23.pgm");
+        rotate180("image24.pgm");
+        rotate180("image26.pgm");
+
+        rotate180("feep.alpha");
+        rotate180("image10_small.alpha");
+        rotate180("image15_small.alpha");
+        rotate180("einstein.alpha");
+        rotate180("image10.alpha");
+        rotate180("image16.alpha");
+        rotate180("image17.alpha");
+        rotate180("image21.alpha");
+        rotate180("image23.alpha");
+        rotate180("image24.alpha");
+        rotate180("image26.alpha");
+    }
+
+};
+
+struct Rotate270 {
+
+    static void test() {
+        rotate270("feep.pgm");
+        rotate270("image10_small.pgm");
+        rotate270("image15_small.pgm");
+        rotate270("einstein.pgm");
+        rotate270("image10.pgm");
+        rotate270("image16.pgm");
+        rotate270("image17.pgm");
+        rotate270("image21.pgm");
+        rotate270("image23.pgm");
+        rotate270("image24.pgm");
+        rotate270("image26.pgm");
+
+        rotate270("feep.alpha");
+        rotate270("image10_small.alpha");
+        rotate270("image15_small.alpha");
+        rotate270("einstein.alpha");
+        rotate270("image10.alpha");
+        rotate270("image16.alpha");
+        rotate270("image17.alpha");
+        rotate270("image21.alpha");
+        rotate270("image23.alpha");
+        rotate270("image24.alpha");
+        rotate270("image26.alpha");
+    }
+
+};
+
+struct ImageMin {
+
+    static void test() {
+
+        min(Image::NW, "feep.alpha", "image10_small.alpha");
+        min(Image::NE, "image26.alpha", "einstein.alpha");
+        min(Image::SW, "image10.alpha", "image16.alpha");
+        min(Image::SE, "image17.alpha", "image21.alpha");
+        min(Image::NE, "image23.alpha", "image24.alpha");
+
+        min(Image::NW, "feep.pgm", "image10_small.pgm");
+        min(Image::NE, "image26.pgm", "einstein.pgm");
+        min(Image::SW, "image10.pgm", "image16.pgm");
+        min(Image::SE, "image17.pgm", "image21.pgm");
+        min(Image::NW, "image23.pgm", "image24.pgm");
+
+        min(Image::NW, "feep.alpha", "image10_small.pgm");
+        min(Image::NE, "image26.alpha", "einstein.pgm");
+        min(Image::SW, "image10.alpha", "image16.pgm");
+        min(Image::SE, "image17.alpha", "image21.pgm");
+        min(Image::SW, "image23.alpha", "image24.pgm");
+
+        min(Image::NW, "feep.pgm", "image10_small.alpha");
+        min(Image::NE, "image26.pgm", "einstein.alpha");
+        min(Image::SW, "image10.pgm", "image16.alpha");
+        min(Image::SE, "image17.pgm", "image21.alpha");
+        min(Image::SE, "image23.pgm", "image24.alpha");
+
+    }
+
+};
+
+struct ImageMax {
+
+    static void test() {
+
+        max(Image::NW, "feep.alpha", "image10_small.alpha");
+        max(Image::NE, "image26.alpha", "einstein.alpha");
+        max(Image::SW, "image10.alpha", "image16.alpha");
+        max(Image::SE, "image17.alpha", "image21.alpha");
+        max(Image::NE, "image23.alpha", "image24.alpha");
+
+        max(Image::NW, "feep.pgm", "image10_small.pgm");
+        max(Image::NE, "image26.pgm", "einstein.pgm");
+        max(Image::SW, "image10.pgm", "image16.pgm");
+        max(Image::SE, "image17.pgm", "image21.pgm");
+        max(Image::NW, "image23.pgm", "image24.pgm");
+
+        max(Image::NW, "feep.alpha", "image10_small.pgm");
+        max(Image::NE, "image26.alpha", "einstein.pgm");
+        max(Image::SW, "image10.alpha", "image16.pgm");
+        max(Image::SE, "image17.alpha", "image21.pgm");
+        max(Image::SW, "image23.alpha", "image24.pgm");
+
+        max(Image::NW, "feep.pgm", "image10_small.alpha");
+        max(Image::NE, "image26.pgm", "einstein.alpha");
+        max(Image::SW, "image10.pgm", "image16.alpha");
+        max(Image::SE, "image17.pgm", "image21.alpha");
+        max(Image::SE, "image23.pgm", "image24.alpha");
+
     }
 
 };
@@ -230,6 +426,27 @@ struct ResizeAlpha : public TestObject {
 int main(int argc, char *argv[]) {
 
     try {
+<<<<<<< HEAD
+        run_test<CreateSmallAlpha>("CreateSmallAlpha", 40);
+        run_test<CreateSmallPGM>("CreateSmallPGM", 40);
+        run_test<CreateLargeAlpha>("CreateLargeAlpha", 2);
+        run_test<CreateLargePGM>("CreateLargePGM", 1);
+
+        run_test<WriteSmallAlpha>("WriteSmallAlpha", 30);
+        run_test<WriteSmallPGM>("WriteSmallPGM", 30);
+        run_test<WriteLargeAlpha>("WriteLargeAlpha", 4);
+        run_test<WriteLargePGM>("WriteLargePGM", 1);
+
+        run_test<Resize>("Resize", 2);
+        run_test<Mirror>("Mirror", 4);
+
+        run_test<Rotate90>("Rotate90", 1);
+        run_test<Rotate180>("Rotate180", 4);
+        run_test<Rotate270>("Rotate270", 1);
+
+        run_test<ImageMin>("Min", 1);
+        run_test<ImageMax>("Max", 1);
+=======
         //CreateSmallAlpha().run_test("CreateSmallAlpha", 100);
         CreateSmallPGM().run_test("CreateSmallPGM", 100);
         CreateLargeAlpha().run_test("CreateLargeAlpha", 1);
@@ -242,6 +459,7 @@ int main(int argc, char *argv[]) {
 
         ResizeAlpha().run_test("ResizeAlpha", 1);
         ResizePGM().run_test("ResizeAlpha", 1);
+>>>>>>> 7324c0bfee34dde45dfc396da16c51f8386d02ce
 
     } catch(std::string &s){
         std::cout << s << '\n';
